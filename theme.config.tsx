336c434b750f71626804a55abcb50985702865d4
AydaMaladec <misakorma12@gmail.com>
2025-06-18 22:54:31 +0300

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default {
   logo: (
-    <img src="/logoe.png" alt="Логотип" width="200" height="200" />
+    <img src="/public/logoe.png" alt="Логотип" width="200" height="200" />
   ),
   project: {
     link: 'https://github.com/твій-юзер/твій-репо',
